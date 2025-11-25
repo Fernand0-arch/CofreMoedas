abd8c6c952368342e50beb89a43c41047a50de9e
Fernando <fernando_st3@hotmail.com>
2025-11-24 22:50:58 -0300

    commit 2

--- src/cofre/moedas/Cofrinho.java
+++ src/cofre/moedas/Cofrinho.java
@@ -6,6 +6,15 @@ public class Cofrinho {
 	
   private ArrayList <Moeda> listaMoedas = new ArrayList <Moeda> ();
 
-
-
+  public void adicionar(Moeda m) {
+    listaMoedas.add(m);
+	}
+  public void remover(Moeda m) {
+    listaMoedas.remove(m);
+    }
+  public void listar() {
+	for (Moeda m : listaMoedas) {
+	  System.out.println(m);
+	}
+    }
 }
